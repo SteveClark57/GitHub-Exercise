9680462a7adf28c6a5506fc14f3e53b9e208fe46
Steve Clark <bukoba.steve@gmail.com>
2024-01-20 13:54:03 +1100

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Stephen Clark on 20/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
